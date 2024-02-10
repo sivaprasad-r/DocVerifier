2ff8a7ee400729391b171c9fc118a03b7dd10802
Sivaprasad R <sivaprasadr91@gmail.com>
2024-02-10 22:53:29 +0530

    Add padding to the button.

--- app/src/main/java/com/example/docverifier/MainActivity.kt
+++ app/src/main/java/com/example/docverifier/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.Button
+import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -37,7 +39,9 @@ fun ScreenContent (){
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        Button(onClick = { /*TODO*/ }) {
+        Button(onClick = { /*TODO*/ },
+            modifier = Modifier.padding(bottom = 20.dp)
+        ) {
             Text("Scan QR")
         }
     }
